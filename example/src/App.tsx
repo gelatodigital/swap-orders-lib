944770663956efdeb4f386f6465450427ebb981c
Harry Trippel <harrytrippel@gmx.de>
2021-11-24 18:56:37 +0100

    add into example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {
-  GelatoLimitOrderPanel,
-  GelatoLimitOrdersHistoryPanel,
-} from "@gelatonetwork/limit-orders-react";
+  GelatoStopLimitOrderPanel,
+  GelatoStopLimitOrdersHistoryPanel,
+} from "@gelatonetwork/stop-limit-orders-react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <GelatoLimitOrderPanel />
-      <GelatoLimitOrdersHistoryPanel />
+      <GelatoStopLimitOrderPanel />
+      <GelatoStopLimitOrdersHistoryPanel />
     </div>
   );
 }
