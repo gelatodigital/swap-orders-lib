06a2db4d97f757725d3437cfc349d202e28b5ef8
Harry Trippel <harrytrippel@gmx.de>
2021-11-25 13:22:52 +0100

    fix typescript

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {
+  GelatoLimitOrderPanel,
+  GelatoLimitOrdersHistoryPanel,
+} from "@gelatonetwork/limit-orders-react";
+import {
   GelatoStopLimitOrderPanel,
-  GelatoStopLimitOrdersHistoryPanel,
+  GelatoStopLimitOrdersHistoryPanel
 } from "@gelatonetwork/stop-limit-orders-react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <GelatoLimitOrderPanel />
+      <GelatoLimitOrdersHistoryPanel />
       <GelatoStopLimitOrderPanel />
       <GelatoStopLimitOrdersHistoryPanel />
     </div>
--- example/src/state/index.tsx
+++ example/src/state/index.tsx
@@ -3,14 +3,16 @@ import { save, load } from 'redux-localstorage-simple'
 import application from './application/reducer'
 import user from './user/reducer'
 import { gelatoReducers, GELATO_PERSISTED_KEYS } from '@gelatonetwork/limit-orders-react'
+import { gelatoReducers as gelatoStopReducers, GELATO_PERSISTED_KEYS as GELATO_STOP_PERSISTED_KEYS } from '@gelatonetwork/stop-limit-orders-react'
 
-const PERSISTED_KEYS: string[] = ['user', ...GELATO_PERSISTED_KEYS]
+const PERSISTED_KEYS: string[] = ['user', ...GELATO_PERSISTED_KEYS, ...GELATO_STOP_PERSISTED_KEYS]
 
 const store = configureStore({
   reducer: {
     application,
     user,
     ...gelatoReducers,
+    ...gelatoStopReducers,
   },
   middleware: [
     // ...getDefaultMiddleware({ thunk: false }),
