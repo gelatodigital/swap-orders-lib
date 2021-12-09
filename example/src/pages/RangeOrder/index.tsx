407746d3aaddccc89ab59007d4c2f3020791addd
0mkar <0mkar@protonmail.com>
2021-12-10 03:32:33 +0530

    range order provider

--- example/src/pages/RangeOrder/index.tsx
+++ example/src/pages/RangeOrder/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
-// import { GelatoLimitOrderPanel, GelatoLimitOrdersHistoryPanel } from '@gelatonetwork/range-orders-react'
+import { GelatoRangeOrderPanel, GelatoProvider } from '@gelatonetwork/range-orders-react'
+import { useActiveWeb3React } from '../../hooks/web3'
+import { useWalletModalToggle } from '../../state/application/hooks'
+
+function Gelato({ children }: { children?: React.ReactNode }) {
+  const { library, chainId, account } = useActiveWeb3React()
+  const toggleWalletModal = useWalletModalToggle()
+
+  return (
+    <GelatoProvider
+      library={library}
+      chainId={chainId}
+      account={account ?? undefined}
+      toggleWalletModal={toggleWalletModal}
+      useDefaultTheme={false}
+    >
+      {children}
+    </GelatoProvider>
+  )
+}
 
 export default function RangeOrder() {
   return (
-    <>
-    </>
+    <Gelato>
+      <GelatoRangeOrderPanel />
+    </Gelato>
   )
 }
--- example/src/pages/StopLimitOrder/index.tsx
+++ example/src/pages/StopLimitOrder/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import { GelatoStopLimitOrderPanel, GelatoStopLimitOrdersHistoryPanel } from '@gelatonetwork/stop-limit-orders-react'
+import { GelatoStopLimitOrderPanel, GelatoStopLimitOrdersHistoryPanel, GelatoProvider } from '@gelatonetwork/stop-limit-orders-react'
+import { useActiveWeb3React } from '../../hooks/web3'
+import { useWalletModalToggle } from '../../state/application/hooks'
+
+function Gelato({ children }: { children?: React.ReactNode }) {
+  const { library, chainId, account } = useActiveWeb3React()
+  const toggleWalletModal = useWalletModalToggle()
+
+  return (
+    <GelatoProvider
+      library={library}
+      chainId={chainId}
+      account={account ?? undefined}
+      toggleWalletModal={toggleWalletModal}
+      useDefaultTheme={false}
+    >
+      {children}
+    </GelatoProvider>
+  )
+}
 
 export default function StopLimitOrder() {
   return (
-    <>
+    <Gelato>
       <GelatoStopLimitOrderPanel />
       <GelatoStopLimitOrdersHistoryPanel />
-    </>
+    </Gelato>
   )
 }
