3a03e61f1cf3127268cae537fef8c51d0070faef
Harry Trippel <harrytrippel@gmx.de>
2021-12-20 14:11:00 +0100

    pending open orders

--- packages/stop-limit-orders-react/src/hooks/gelato/useGelatoStopLimitOrdersHistory.ts
+++ packages/stop-limit-orders-react/src/hooks/gelato/useGelatoStopLimitOrdersHistory.ts
@@ -87,7 +87,7 @@ export default function useGelatoStopLimitOrdersHistory(
             (order) => order.status === "open"
           );
 
-          const pendingOrdersLS = getLSOrders(chainId, account);
+          const pendingOrdersLS = getLSOrders(chainId, account, true);
 
           setOpenOrders({
             confirmed: openOrdersLS
