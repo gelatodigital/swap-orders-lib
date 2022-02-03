1c8e9989372a6a40ea609ba7fa34e495f59af92d
Harry Trippel <harrytrippel@gmx.de>
2022-02-03 15:20:01 +0100

    formatting

--- packages/stop-limit-orders-react/src/components/order/SwapModalHeader.tsx
+++ packages/stop-limit-orders-react/src/components/order/SwapModalHeader.tsx
@@ -94,9 +94,9 @@ export default function SwapModalHeader({
     () =>
       minReturn && minReturn.greaterThan(0) && inputAmount
         ? new Price({
-          quoteAmount: minReturn,
-          baseAmount: inputAmount,
-        })
+            quoteAmount: minReturn,
+            baseAmount: inputAmount,
+          })
         : undefined,
     [inputAmount, minReturn]
   );
@@ -131,7 +131,7 @@ export default function SwapModalHeader({
                 fontWeight={500}
                 color={
                   showAcceptChanges &&
-                    trade?.tradeType === TradeType.EXACT_OUTPUT
+                  trade?.tradeType === TradeType.EXACT_OUTPUT
                     ? theme.primary1
                     : ""
                 }
