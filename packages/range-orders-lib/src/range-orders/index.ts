07769ad3f3dc068b5759f418b5e0c9bde8df677e
Gauddel <matthieumariejoseph@gmail.com>
2022-02-25 14:58:14 +0100

    add getAmountsIn function

--- packages/range-orders-lib/src/range-orders/index.ts
+++ packages/range-orders-lib/src/range-orders/index.ts
@@ -31,7 +31,7 @@ import {
 } from "ethers";
 import { sqrt } from "@uniswap/sdk-core";
 import JSBI from "jsbi";
-import { TickMath } from "@uniswap/v3-sdk";
+import { FullMath, maxLiquidityForAmounts, TickMath } from "@uniswap/v3-sdk";
 import { PayableOverrides } from "@ethersproject/contracts";
 
 export const isValidChainIdAndHandler = (
@@ -454,6 +454,119 @@ export class GelatoRangeOrder {
         : rangeOrderData.expiryTime.sub(now);
     }
     return null;
+  }
+
+  public getAmountIn(
+    currentTick: BigNumber,
+    lowerTick: BigNumber,
+    upperTick: BigNumber,
+    amount0: BigNumber,
+    amount1: BigNumber,
+    sqrtPriceX96: BigNumber
+  ): { amount0: BigNumber; amount1: BigNumber } {
+    const liquidity = maxLiquidityForAmounts(
+      JSBI.BigInt(currentTick.toString()),
+      JSBI.BigInt(lowerTick.toString()),
+      JSBI.BigInt(upperTick.toString()),
+      JSBI.BigInt(amount0.toString()),
+      JSBI.BigInt(amount1.toString()),
+      true
+    );
+
+    if (BigNumber.from(liquidity.toString()).isZero())
+      return { amount0: ethers.constants.Zero, amount1: ethers.constants.Zero };
+
+    amount0 = ethers.constants.Zero;
+    amount1 = ethers.constants.Zero;
+    ƒ;
+    if (currentTick.lt(lowerTick)) {
+      amount0 = BigNumber.from(
+        this.getAmount0Delta(
+          TickMath.getSqrtRatioAtTick(lowerTick.toNumber()),
+          TickMath.getSqrtRatioAtTick(upperTick.toNumber()),
+          liquidity,
+          false
+        ).toString()
+      );
+    } else if (currentTick.lt(upperTick)) {
+      amount0 = BigNumber.from(
+        this.getAmount0Delta(
+          JSBI.BigInt(sqrtPriceX96.toString()),
+          TickMath.getSqrtRatioAtTick(upperTick.toNumber()),
+          liquidity,
+          false
+        ).toString()
+      );
+
+      amount1 = BigNumber.from(
+        this.getAmount1Delta(
+          TickMath.getSqrtRatioAtTick(lowerTick.toNumber()),
+          JSBI.BigInt(sqrtPriceX96.toString()),
+          liquidity,
+          false
+        ).toString()
+      );
+    } else {
+      amount1 = BigNumber.from(
+        this.getAmount1Delta(
+          TickMath.getSqrtRatioAtTick(lowerTick.toNumber()),
+          TickMath.getSqrtRatioAtTick(upperTick.toNumber()),
+          liquidity,
+          false
+        ).toString()
+      );
+    }
+
+    return { amount0, amount1 };
+  }
+
+  public getAmount0Delta(
+    sqrtRatioAX96: JSBI,
+    sqrtRatioBX96: JSBI,
+    liquidity: JSBI,
+    roundUp: boolean
+  ): JSBI {
+    if (JSBI.greaterThan(sqrtRatioAX96, sqrtRatioBX96)) {
+      [sqrtRatioAX96, sqrtRatioBX96] = [sqrtRatioBX96, sqrtRatioAX96];
+    }
+
+    const numerator1 = JSBI.leftShift(liquidity, JSBI.BigInt(96));
+    const numerator2 = JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96);
+
+    return roundUp
+      ? FullMath.mulDivRoundingUp(
+          FullMath.mulDivRoundingUp(numerator1, numerator2, sqrtRatioBX96),
+          JSBI.BigInt(1),
+          sqrtRatioAX96
+        )
+      : JSBI.divide(
+          JSBI.divide(JSBI.multiply(numerator1, numerator2), sqrtRatioBX96),
+          sqrtRatioAX96
+        );
+  }
+
+  public getAmount1Delta(
+    sqrtRatioAX96: JSBI,
+    sqrtRatioBX96: JSBI,
+    liquidity: JSBI,
+    roundUp: boolean
+  ): JSBI {
+    if (JSBI.greaterThan(sqrtRatioAX96, sqrtRatioBX96)) {
+      [sqrtRatioAX96, sqrtRatioBX96] = [sqrtRatioBX96, sqrtRatioAX96];
+    }
+
+    const Q96 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(96));
+
+    return roundUp
+      ? FullMath.mulDivRoundingUp(
+          liquidity,
+          JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96),
+          Q96
+        )
+      : JSBI.divide(
+          JSBI.multiply(liquidity, JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96)),
+          Q96
+        );
   }
 
   public async encodeRangeOrderSubmission(
