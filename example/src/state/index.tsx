2d391825cf1694b5f54360325b44c40ba3ccc540
Harry Trippel <harrytrippel@gmx.de>
2022-03-04 14:13:02 +0100

    reset exmaple repo

--- example/src/state/index.tsx
+++ example/src/state/index.tsx
@@ -3,7 +3,7 @@ import { save, load } from 'redux-localstorage-simple'
 import application from './application/reducer'
 import user from './user/reducer'
 import { gelatoReducers, GELATO_PERSISTED_KEYS } from '@gelatonetwork/limit-orders-react'
-import { gelatoStopLimitReducers as gelatoStopReducers, GELATO_STOPLIMIT_PERSISTED_KEYS as GELATO_STOP_PERSISTED_KEYS } from '@gelatonetwork/stop-limit-orders-react'
+import { gelatoReducers as gelatoStopReducers, GELATO_PERSISTED_KEYS as GELATO_STOP_PERSISTED_KEYS } from '@gelatonetwork/stop-limit-orders-react'
 
 const PERSISTED_KEYS: string[] = ['user', ...GELATO_PERSISTED_KEYS, ...GELATO_STOP_PERSISTED_KEYS]
 
