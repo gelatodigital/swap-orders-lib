df4e0725061b94396c6f1ef215dffc7a04509121
Harry Trippel <harrytrippel@gmx.de>
2022-03-31 17:29:55 +0200

    formatting

--- packages/limit-orders-lib/src/constants/index.ts
+++ packages/limit-orders-lib/src/constants/index.ts
@@ -154,7 +154,7 @@ export const SUBGRAPH_URL = {
     "https://api.thegraph.com/subgraphs/name/gelatodigital/limit-orders-bsc",
   [CHAIN_ID.AVAX]:
     "https://api.thegraph.com/subgraphs/name/gelatodigital/limit-orders-avax",
-  [CHAIN_ID.CRONOS]: "https://graph.vvs.finance/limit-orders-test/graphql",
+  [CHAIN_ID.CRONOS]: "https://graph.vvs.finance/limit-orders-test",
 };
 
 export const GELATO_LIMIT_ORDERS_ADDRESS = {
